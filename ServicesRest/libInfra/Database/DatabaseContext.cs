03fb2f69a24572990263f732fae558e6b88d94d9
Gabriel Moré <gabriel_more@hotmail.com>
2019-09-06 16:44:48 -0300

    Modificações estruturais no projeto

---
+++ ServicesRest/libInfra/Database/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace libInfra.Database
+{
+    class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("")
+        {
+
+        }
+    }
+}
