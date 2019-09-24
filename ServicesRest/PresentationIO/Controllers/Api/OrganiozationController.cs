59405d863266b939e7bd67e3a20da6819fdfecd8
Gabriel Moré <gabriel_more@hotmail.com>
2019-09-23 21:43:03 -0300

    Ajustado o bug de depêndencias - Migrations rodando

--- ServicesRest/PresentationIO/Controllers/Api/OrganiozationController.cs
+++ ServicesRest/PresentationIO/Controllers/Api/OrganiozationController.cs
@@ -10,129 +10,128 @@ using System.Web.Http;
 
 namespace API.Controllers.Api
 {
-    [Authorize]
+    
     [RoutePrefix("api/Organization")]
     public class OrganiozationController : ApiController
     {
-        public class UsersController : ApiController
+        [Route("GetAll")]
+        [AcceptVerbs("GET")]
+        public HttpResponseMessage GetAll(HttpRequestMessage request)
         {
-            [Route("GetAll")]
-            [AcceptVerbs("GET")]
-            public HttpResponseMessage GetAll(HttpRequestMessage request)
+            try
             {
-                try
+                List<Organization> organizations;
+                using (OrganizationRepository repository = new OrganizationRepository())
                 {
-                    List<Organization> organizations;
-                    using (OrganizationRepository repository = new OrganizationRepository())
-                    {
-                        organizations = repository.GetAll().ToList();
-                    }
-                    return request.CreateResponse<List<Organization>>(HttpStatusCode.OK, organizations);
+                    organizations = repository.GetAll().ToList();
                 }
-                catch (Exception e)
-                {
-                    return request.CreateErrorResponse(HttpStatusCode.BadRequest, "Não foi possível listar as organizações [" + e.Message + "]");
-                }
+                return request.CreateResponse<List<Organization>>(HttpStatusCode.OK, organizations);
             }
-            [AcceptVerbs("GET")]
-            [Route("Find/Contact/{id}")]
-            public HttpResponseMessage FindContacts(HttpRequestMessage request, int? id)
+            catch (Exception e)
             {
-                try{
-                    List<Contact> contacts;
-                    using (ContactRepository repository = new ContactRepository())
-                    {
-                        contacts = repository.Get((c => c.Organization.ID == id)).ToList<Contact>();
-                    }
-                    return request.CreateResponse<List<Contact>>(HttpStatusCode.OK, contacts);
-                }
-                catch (Exception e)
-                {
-                    return request.CreateErrorResponse(HttpStatusCode.BadRequest, e.Message);
-                }
+                return request.CreateErrorResponse(HttpStatusCode.BadRequest, "Não foi possível listar as organizações [" + e.Message + "]");
             }
+        }
+        [AcceptVerbs("GET")]
+        [Route("Find/Contact/{id}")]
+        public HttpResponseMessage FindContacts(HttpRequestMessage request, int? id)
+        {
+            try
+            {
+                List<Contact> contacts;
+                using (ContactRepository repository = new ContactRepository())
+                {
+                    contacts = repository.Get((c => c.Organization.ID == id)).ToList<Contact>();
+                }
+                return request.CreateResponse<List<Contact>>(HttpStatusCode.OK, contacts);
+            }
+            catch (Exception e)
+            {
+                return request.CreateErrorResponse(HttpStatusCode.BadRequest, e.Message);
+            }
+        }
 
 
-            [AcceptVerbs("GET")]
-            [Route("Find/{id}")]
-            public HttpResponseMessage Find(HttpRequestMessage request, int id)
+        [AcceptVerbs("GET")]
+        [Route("Find/{id}")]
+        public HttpResponseMessage Find(HttpRequestMessage request, int id)
+        {
+            try
             {
-                try
+                using (OrganizationRepository repository = new OrganizationRepository())
                 {
-                    using (OrganizationRepository repository = new OrganizationRepository())
+                    Organization org = repository.Find(id);
+                    if (org != null)
                     {
-                        Organization org = repository.Find(id);
-                        if (org != null)
-                        {
-                            return request.CreateResponse<Organization>(HttpStatusCode.OK, org);
-                        }
-                        else
-                        {
-                            return request.CreateErrorResponse(HttpStatusCode.BadRequest, "Organização não pode ser encontrada");
-                        }
+                        return request.CreateResponse<Organization>(HttpStatusCode.OK, org);
                     }
-                }
-                catch (Exception e)
-                {
-                    return request.CreateErrorResponse(HttpStatusCode.BadRequest, e.Message);
+                    else
+                    {
+                        return request.CreateErrorResponse(HttpStatusCode.BadRequest, "Organização não pode ser encontrada");
+                    }
                 }
             }
-            public HttpResponseMessage Add(HttpRequestMessage request, Organization org)
+            catch (Exception e)
             {
-                try
-                {
-                    using (OrganizationRepository repository = new OrganizationRepository())
-                    {
-                        repository.Add(org);
-                        repository.SaveAll();
-                    }
-                    return request.CreateResponse<Organization>(HttpStatusCode.OK, org);
-                }
-                catch (Exception e)
-                {
-                    return request.CreateErrorResponse(HttpStatusCode.BadRequest, "Não foi possível inserir usuário [" + e.Message + "]");
-                }
+                return request.CreateErrorResponse(HttpStatusCode.BadRequest, e.Message);
             }
-
-
-            [Route("Update")]
-            [AcceptVerbs("POST")]
-            public HttpResponseMessage Update(HttpRequestMessage request, Organization org)
+        }
+        public HttpResponseMessage Add(HttpRequestMessage request, Organization org)
+        {
+            try
             {
-                try
+                using (OrganizationRepository repository = new OrganizationRepository())
                 {
-                    using (OrganizationRepository repository = new OrganizationRepository())
-                    {
-                        repository.Update(org);
-                        repository.SaveAll();
-                    }
-                    return request.CreateResponse<Organization>(HttpStatusCode.OK, org);
+                    repository.Add(org);
+                    repository.SaveAll();
                 }
-                catch (Exception e)
-                {
-                    return request.CreateErrorResponse(HttpStatusCode.BadRequest, e.Message);
-                }
+                return request.CreateResponse<Organization>(HttpStatusCode.OK, org);
             }
-
-            [Route("Delete/{id}")]
-            [AcceptVerbs("DELETE")]
-            public HttpResponseMessage Delete(HttpRequestMessage request, int id)
+            catch (Exception e)
             {
-                try
+                return request.CreateErrorResponse(HttpStatusCode.BadRequest, "Não foi possível inserir usuário [" + e.Message + "]");
+            }
+        }
+
+
+        [Route("Update")]
+        [AcceptVerbs("POST")]
+        public HttpResponseMessage Update(HttpRequestMessage request, Organization org)
+        {
+            try
+            {
+                using (OrganizationRepository repository = new OrganizationRepository())
                 {
-                    using (OrganizationRepository rep = new OrganizationRepository())
-                    {
-                        Func<Organization, bool> delete = (o => o.ID == id);
-                        rep.Delete(delete);
-                        rep.SaveAll();
-                    }
-                    return request.CreateResponse(HttpStatusCode.Accepted);
+                    repository.Update(org);
+                    repository.SaveAll();
                 }
-                catch (Exception e)
+                return request.CreateResponse<Organization>(HttpStatusCode.OK, org);
+            }
+            catch (Exception e)
+            {
+                return request.CreateErrorResponse(HttpStatusCode.BadRequest, e.Message);
+            }
+        }
+
+        [Route("Delete/{id}")]
+        [AcceptVerbs("DELETE")]
+        public HttpResponseMessage Delete(HttpRequestMessage request, int id)
+        {
+            try
+            {
+                using (OrganizationRepository rep = new OrganizationRepository())
                 {
-                    return request.CreateErrorResponse(HttpStatusCode.BadRequest, e.Message);
+                    Func<Organization, bool> delete = (o => o.ID == id);
+                    rep.Delete(delete);
+                    rep.SaveAll();
                 }
+                return request.CreateResponse(HttpStatusCode.Accepted);
+            }
+            catch (Exception e)
+            {
+                return request.CreateErrorResponse(HttpStatusCode.BadRequest, e.Message);
             }
         }
     }
 }
+
